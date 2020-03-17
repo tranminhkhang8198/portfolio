e4b93c1556afa6afefa6f64c53ce632b98a6a9ce
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-03-17 15:56:04 +0700

    Initial timeline for projects

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ $(document).ready(function(e) {
     $win = $(window);
     $navbar = $('#header');
     $toggle = $('.toggle-button');
+    $mouse = $('#mouse_icon');
     var width = $navbar.width();
 
     // resize event
     $win.resize(() => {
         toggle_onclick($win, $navbar, width);
+    });
+
+
+    // scroll detect
+    window.addEventListener("scroll", function() {
+        if (window.pageYOffset === 0) {
+            $mouse.css("display", "block");
+        } else {
+            $mouse.css("display", "none");
+        }
     });
 
     $toggle.click(function(e) {
