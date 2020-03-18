5b7fa8a850e37dda1a91f82c11c8fb17e30aa836
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-03-18 21:46:40 +0700

    Update template for projects demo

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 $(document).ready(function(e) {
-    $win = $(window);
-    $navbar = $('#header');
-    $toggle = $('.toggle-button');
-    $mouse = $('#mouse_icon');
-    var width = $navbar.width();
+  $win = $(window);
+  $navbar = $("#header");
+  $toggle = $(".toggle-button");
+  $mouse = $("#mouse_icon");
+  var width = $navbar.width();
 
-    // resize event
-    $win.resize(() => {
-        toggle_onclick($win, $navbar, width);
-    });
+  // resize event
+  $win.resize(() => {
+    toggle_onclick($win, $navbar, width);
+  });
 
+  // scroll detect
+  window.addEventListener("scroll", function() {
+    if (window.pageYOffset === 0) {
+      $mouse.css("display", "block");
+    } else {
+      $mouse.css("display", "none");
+    }
+  });
 
-    // scroll detect
-    window.addEventListener("scroll", function() {
-        if (window.pageYOffset === 0) {
-            $mouse.css("display", "block");
-        } else {
-            $mouse.css("display", "none");
-        }
-    });
-
-    $toggle.click(function(e) {
-        $navbar.toggleClass("toggle-left");
-    });
+  $toggle.click(function(e) {
+    $navbar.toggleClass("toggle-left");
+  });
 });
 
 function toggle_onclick($win, $navbar, width) {
-    if ($win.width() <= 768) {
-        $navbar.css({ left: `-${width}px` });
-    } else {
-        $navbar.css({ left: '0px' })
-    }
+  if ($win.width() <= 768) {
+    $navbar.css({ left: `-${width}px` });
+  } else {
+    $navbar.css({ left: "0px" });
+  }
 }
 
-var typed = new Typed('#typed', {
-    strings: [
-        'Web Developer',
-        'Freelancer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
+var typed = new Typed("#typed", {
+  strings: ["Web Developer", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
 });
 
-var typed_2 = new Typed('#typed_2', {
-    strings: [
-        'Web Developer',
-        'Freelancer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
+var typed_2 = new Typed("#typed_2", {
+  strings: ["Web Developer", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
 });
