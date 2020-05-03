2308a1d119256ebbd3b543d5516eb73c1a7c39cb
Minh Khang <minhkhang811998@gmail.com>
2020-05-03 23:56:53 +0700

    Adding transcript

--- index.js
+++ index.js
@@ -1,106 +1,104 @@
 $(document).ready(function (e) {
-    $win = $(window);
-    $navbar = $('#header');
-    $toggle = $('.toggle-button');
-    var width = $navbar.width();
-    $mouse = $("#mouse_icon");
+  $win = $(window);
+  $navbar = $("#header");
+  $toggle = $(".toggle-button");
+  var width = $navbar.width();
+  $mouse = $("#mouse_icon");
 
-    console.log(width);
+  console.log(width);
 
+  toggle_onclick($win, $navbar, width);
+
+  // resize event
+  $win.resize(function () {
     toggle_onclick($win, $navbar, width);
+  });
 
-    // resize event
-    $win.resize(function () {
-        toggle_onclick($win, $navbar, width);
-    });
+  $toggle.click(function (e) {
+    $navbar.toggleClass("toggle-left");
+  });
 
-    $toggle.click(function (e) {
-        $navbar.toggleClass("toggle-left");
-    });
-
-    // scroll detect
-    window.addEventListener("scroll", function () {
-        if (window.pageYOffset === 0) {
-            $mouse.css("display", "block");
-        } else {
-            $mouse.css("display", "none");
-        }
-    });
-
-    // stop video
-    function pauseAllVideos() {
-        $('iframe').contents().find('video').each(function () {
-            this.pause();
-        });
-        $('video').each(function () {
-            this.pause();
-        });
+  // scroll detect
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset === 0) {
+      $mouse.css("display", "block");
+    } else {
+      $mouse.css("display", "none");
     }
+  });
 
-    // Gets the video src from the data-src on each button
-    var $videoSrc;
-    $('.video-btn').click(function () {
-        $videoSrc = $(this).data("src");
+  // stop video
+  function pauseAllVideos() {
+    $("iframe")
+      .contents()
+      .find("video")
+      .each(function () {
+        this.pause();
+      });
+    $("video").each(function () {
+      this.pause();
     });
+  }
 
-    // when the modal is opened autoplay it  
-    $('#myModal').on('shown.bs.modal', function (e) {
+  // Gets the video src from the data-src on each button
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
 
-        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
+  // when the modal is opened autoplay it
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
 
-    // stop playing the youtube video when I close the modal
-    $('#myModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
-        // $("#video").attr('src', $videoSrc);
-        pauseAllVideos();
-    });
+  // stop playing the youtube video when I close the modal
+  $("#myModal").on("hide.bs.modal", function (e) {
+    // a poor man's stop video
+    // $("#video").attr('src', $videoSrc);
+    pauseAllVideos();
+  });
 });
 
 function toggle_onclick($win, $navbar, width) {
-    if ($win.width() <= 768) {
-        $navbar.css({
-            left: `-${width}px`
-        });
-    } else {
-        $navbar.css({
-            left: '0px'
-        });
-    }
+  if ($win.width() <= 768) {
+    $navbar.css({
+      left: `-${width}px`,
+    });
+  } else {
+    $navbar.css({
+      left: "0px",
+    });
+  }
 }
 
-var typed = new Typed('#typed', {
-    strings: [
-        'Web Developer',
-        'Freelancer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
+var typed = new Typed("#typed", {
+  strings: ["Web Developer", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
 });
 
-var typed_2 = new Typed('#typed_2', {
-    strings: [
-        'Web Developer',
-        'Freelancer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
+var typed_2 = new Typed("#typed_2", {
+  strings: ["Web Developer", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
 });
 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  if (anchor.getAttribute("href").startsWith("#demo")) {
+    return;
+  }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    if (anchor.getAttribute("href").startsWith("#demo")) {
-        return;
-    }
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
